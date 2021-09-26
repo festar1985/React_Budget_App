4c984bbd95da0028ed20acc81ef8eda251ca0240
Georgi <georgi.k.konstantinov@gmail.com>
2021-09-27 01:11:38 +0200

    Constinue developing

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,7 @@
 import React from "react";
-
+import moment from "moment";
 import { Link } from "react-router-dom";
+import numeral from "numeral";
 
 const ExpensesListItem = ({ id, description, category, amount, createdAt }) => (
   <div>
@@ -8,8 +9,8 @@ const ExpensesListItem = ({ id, description, category, amount, createdAt }) => (
       <h3>{description}</h3>
     </Link>
     <p>{category}</p>
-    <p>{amount}</p>
-    <p>{createdAt}</p>
+    <p>{numeral(amount / 100).format("0,0.00")}</p>
+    <p>{moment(createdAt).format("DD.MMM.YYYY")}</p>
   </div>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,27 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import "./styles/style.scss";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./redux/store/configureStore";
-import { addExpense } from "./redux/actions/expenses";
+
+import "normalize.css";
 import "react-dates/lib/css/_datepicker.css";
+import "./styles/style.scss";
 
 const store = configureStore();
-
-store.dispatch(addExpense({ description: "Rent", amount: 100 }));
-
-store.dispatch(
-  addExpense({ description: "Gass Bill", amount: 800, createdAt: 100 })
-);
-
-store.dispatch(
-  addExpense({
-    description: "Water Bill",
-    amount: 2800,
-    note: "imalo edno vreme",
-  })
-);
 
 const jsx = (
   <Provider store={store}>
