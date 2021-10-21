26fa3fe2121c332f38048aac93c8ac288cf33088
Georgi <georgi.k.konstantinov@gmail.com>
2021-10-21 10:15:43 +0200

    firebase config added

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,9 +2,18 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref } from "firebase/database";
 import { GoogleAuthProvider } from "firebase/auth";
 
-import config from "../firebase/firebaseConfig.json";
+const envConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDERID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
 
-const app = initializeApp(config);
+const app = initializeApp(envConfig);
 
 const googleProvider = new GoogleAuthProvider();
 
